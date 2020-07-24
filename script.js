6ee89b0f0d2b96c1ef0b890685e47975d8b80510
Emmanuel99 <emuhembrom@gmail.com>
2020-07-25 03:20:53 +0600

    Case part done

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 //Amount of accessories
 //decreasing amount and cost of accessories
-function decreaseAccessories() {
-    currentNumber = document.getElementById("phoneAmount").value;
+function decreaseAccessories(minus,minusCost,price) {
+    currentNumber = minus.value;
     currentNumber--;
-    document.getElementById("phoneAmount").value = currentNumber;
-    let currentCost = document.getElementById("mobileCost").innerHTML;
+    minus.value = currentNumber;
+    let currentCost = minusCost.innerHTML;
     currentCostNumber = parseFloat(currentCost);
     let totalCost = currentCostNumber;
-    totalCost = totalCost-1219;
-    document.getElementById("mobileCost").innerHTML = totalCost;
+    totalCost = totalCost-price;
+    minusCost.innerHTML = totalCost;
 }
 //increasing amount and cost of accessories
-function increaseAccessories() {
-    currentNumber = document.getElementById("phoneAmount").value;
+function increaseAccessories(plus,plusCost,price) {
+    currentNumber = plus.value;
     currentNumber++;
-    document.getElementById("phoneAmount").value = currentNumber;
-    let currentCost = document.getElementById("mobileCost").innerHTML;
+    plus.value = currentNumber;
+    let currentCost = plusCost.innerHTML;
     currentCostNumber = parseFloat(currentCost);
     let totalCost = currentCostNumber;
-    totalCost = totalCost+1219;
-    document.getElementById("mobileCost").innerHTML = totalCost;
+    totalCost = totalCost+price;
+    plusCost.innerHTML = totalCost;
 }
-
+//common function for mobile phone cost adding and deducting 
+let phoneNumber = document.getElementById("phoneAmount");
+let totalCost = document.getElementById("mobileCost");
+let perCost = 1219;
 //Decreasing of phone 
 document.getElementById("phoneDecrease").addEventListener("click",function(){
-    decreaseAccessories();
+    decreaseAccessories(phoneNumber,totalCost,perCost);
 })
 //Increasing of phone
 document.getElementById("phoneIncrease").addEventListener("click",function(){
-    increaseAccessories();
+    increaseAccessories(phoneNumber,totalCost,perCost);
+})
+
+//function for phone case 
+//common function for phone case 
+
+let phoneCaseNumber = document.getElementById("caseNumber");
+let totalCaseCost = document.getElementById("caseCost");
+let perCaseCost = 59;
+
+//decreasing of case
+document.getElementById("decreaseCase").addEventListener("click",function() {
+    decreaseAccessories(phoneCaseNumber,totalCaseCost,perCaseCost);    
+})
+
+//Increasing of case 
+document.getElementById("increaseCase").addEventListener("click",function() {
+    increaseAccessories(phoneCaseNumber,totalCaseCost,perCaseCost);    
 })
