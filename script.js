56a8b33856d88be078c12e7783a39030a3e03891
Emmanuel99 <emuhembrom@gmail.com>
2020-07-26 00:04:29 +0600

    button problem fixed

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 //Amount of accessories
 //decreasing amount and cost of accessories
-function decreaseAccessories(minus,minusCost,price) {
+function decreaseAccessories(minus, minusCost, price) {
     currentNumber = minus.value;
-    currentNumber--;
-    minus.value = currentNumber;
-    let currentCost = minusCost.innerHTML;
-    currentCostNumber = parseFloat(currentCost);
-    let totalCost = currentCostNumber;
-    totalCost = totalCost-price;
-    minusCost.innerHTML = totalCost;
+    if(currentNumber<=0){
+        minus.innerHTML=0;
+    }
+    else{
+        currentNumber--;
+        minus.value = currentNumber;
+        let currentCost = minusCost.innerHTML;
+        currentCostNumber = parseFloat(currentCost);
+        let totalCost = currentCostNumber;
+        totalCost = totalCost - price;
+        minusCost.innerHTML = totalCost;
+    }
 }
 //increasing amount and cost of accessories
-function increaseAccessories(plus,plusCost,price) {
+function increaseAccessories(plus, plusCost, price) {
     currentNumber = plus.value;
     currentNumber++;
     plus.value = currentNumber;
     let currentCost = plusCost.innerHTML;
     currentCostNumber = parseFloat(currentCost);
     let totalCost = currentCostNumber;
-    totalCost = totalCost+price;
+    totalCost = totalCost + price;
     plusCost.innerHTML = totalCost;
 }
 //common function for mobile phone cost adding and deducting 
@@ -26,12 +31,12 @@ let phoneNumber = document.getElementById("phoneAmount");
 let totalCost = document.getElementById("mobileCost");
 let perCost = 1219;
 //Decreasing of phone 
-document.getElementById("phoneDecrease").addEventListener("click",function(){
-    decreaseAccessories(phoneNumber,totalCost,perCost);
+document.getElementById("phoneDecrease").addEventListener("click", function () {
+    decreaseAccessories(phoneNumber, totalCost, perCost);
 })
 //Increasing of phone
-document.getElementById("phoneIncrease").addEventListener("click",function(){
-    increaseAccessories(phoneNumber,totalCost,perCost);
+document.getElementById("phoneIncrease").addEventListener("click", function () {
+    increaseAccessories(phoneNumber, totalCost, perCost);
 })
 
 //function for phone case 
@@ -42,11 +47,11 @@ let totalCaseCost = document.getElementById("caseCost");
 let perCaseCost = 59;
 
 //decreasing of case
-document.getElementById("decreaseCase").addEventListener("click",function() {
-    decreaseAccessories(phoneCaseNumber,totalCaseCost,perCaseCost);    
+document.getElementById("decreaseCase").addEventListener("click", function () {
+    decreaseAccessories(phoneCaseNumber, totalCaseCost, perCaseCost);
 })
 
 //Increasing of case 
-document.getElementById("increaseCase").addEventListener("click",function() {
-    increaseAccessories(phoneCaseNumber,totalCaseCost,perCaseCost);    
+document.getElementById("increaseCase").addEventListener("click", function () {
+    increaseAccessories(phoneCaseNumber, totalCaseCost, perCaseCost);
 })
